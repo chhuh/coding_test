e0b4770e1cc0811b8b8b8a5f44785f9e15f301c0
chhuh <huh1125@ajou.ac.kr>
2021-05-30 15:29:54 +0900

    code rework

--- Dijkstra/프로그래머스_배달.py
+++ Dijkstra/프로그래머스_배달.py
@@ -1,36 +1,45 @@
-import math
+import sys
 
 def solution(N, road, K):
     answer = 0
-    visited = []
-    distance = [0] + [math.inf for i in range(1,N)]
+    
     roads = {i : {} for i in range(N)}
-    # 주어진 road로 초기 roads작성
+    
+    # 초기 roads작성
     for r in road:
-        if r[1] - 1 in roads and r[0] - 1 in roads[r[1] - 1]:
-            if roads[r[1] - 1][r[0] - 1] > r[2]:
-                roads[r[1] - 1][r[0] - 1] = r[2]
+        if r[0]-1 in roads and r[1]-1 in roads[r[0] - 1]:
+            if r[2] < roads[r[0] - 1][r[1] - 1]:
                 roads[r[0] - 1][r[1] - 1] = r[2]
+                roads[r[1] - 1][r[0] - 1] = r[2]       
         else:
-            roads[r[1] - 1][r[0] - 1] = r[2]
             roads[r[0] - 1][r[1] - 1] = r[2]
-
+            roads[r[1] - 1][r[0] - 1] = r[2]
+    
+    # 0을 기준으로 한 초기 distance작성
+    distance = [0] + [sys.maxsize for _ in range(N-1)]
+    
     for i in roads[0]:
         distance[i] = roads[0][i]
-        
+    
+    # 0을 기준으로 최단거리 검거된 마을 모임
+    visited = []
+    
     while len(visited) != N:
-        minimum = math.inf
+        mini = sys.maxsize
+        
         for i in range(1, N):
-            if i not in visited and distance[i] < minimum:
-                minimum = distance[i]
+            if i not in visited and distance[i] < mini:
+                mini  = distance[i]
                 town = i
-                
+        
         visited.append(town)
+        
         for i in roads[town]:
-            if distance[i] > distance[town] + roads[town][i]:
-                distance[i] = distance[town] + roads[town][i]
-
+            if distance[i] > roads[town][i] + mini:
+                distance[i] = roads[town][i] + mini
+                
     for d in distance:
         if d <= K:
-            answer += 1                
+            answer += 1
+        
     return answer
