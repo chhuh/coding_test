cb1f0e72fee5adeb20495d7870555f73e226cbee
chhuh <huh1125@ajou.ac.kr>
2021-06-12 13:32:32 +0900

    new code

---
+++ stackNqueue/프로그래머스_기능개발.py
@@ -0,0 +1,29 @@
+import math
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    release_date = []
+    
+    for i in range(0, len(progresses)):
+        release_date.append(math.ceil((100 - progresses[i]) / speeds[i]))
+        
+    deque_release_date = deque(release_date)    
+    max = 0
+    num = 1
+    
+    while deque_release_date:
+        temp = deque_release_date.popleft()
+        if(max < temp):
+            if max == 0:
+                max = temp
+            else:
+                max = temp
+                answer.append(num)
+                num = 1
+        else:
+            num += 1
+            
+    answer.append(num)
+    
+    return answer
