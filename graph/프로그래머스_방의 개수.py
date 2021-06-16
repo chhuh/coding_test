a456de0217a20e2ef21c0f83b02c010d3367b42e
chhuh <huh1125@ajou.ac.kr>
2021-06-16 16:04:45 +0900

    new code

---
+++ graph/프로그래머스_방의 개수.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from collections import deque
+
+def solution(arrows):
+    answer = 0
+    
+    move = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    
+    now = (0,0)
+    queue = deque([now])
+    
+    visited = defaultdict(int)
+    visited_dir = defaultdict(int)
+    
+    for arrow in arrows:
+        for _ in range(2):
+            new = (now[0] + move[arrow][0], now[1] + move[arrow][1])
+            queue.append(new)
+            now = new
+    
+    now = queue.popleft()
+    visited[now] = 1
+    
+    while queue:
+        new = queue.popleft()
+        if visited[new] == 1:
+            if visited_dir[(now, new)] == 0:
+                answer += 1
+        else:
+            visited[new] = 1
+        
+        visited_dir[(now, new)] = 1
+        visited_dir[(new, now)] = 1
+        now = new
+        
+    return answer
