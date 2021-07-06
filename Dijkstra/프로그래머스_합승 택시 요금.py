ab35e2597359d321849616bc0916cffc0cf9267a
chhuh <huh1125@ajou.ac.kr>
2021-07-06 15:48:14 +0900

    new

--- Dijkstra/프로그래머스_합승 택시 요금.py
+++ Dijkstra/프로그래머스_합승 택시 요금.py
@@ -1,48 +1,84 @@
 import sys
 import heapq
 
-# dijkstra
-def dijkstra(src, dst):
-    global graph, length
+# def dij(src, dst):
+#     global graph, length
     
-    visit = [sys.maxsize]*(length+1)
+#     my_fare = [sys.maxsize for _ in range(length)]
+#     my_fare[src] = 0
+#     hpq = [[0, src]]
+#     heapq.heapify(hpq)
     
-    visit[src] = 0
-    
-    hpq = [[0, src]]
-    
-    heapq.heapify(hpq)
-    
-    #bfs
-    while hpq:
-        cost, node = heapq.heappop(hpq)
+#     while hpq:
+#         fare, node = heapq.heappop(hpq)
         
-        if cost > visit[node]:
-            continue
-        
-        for g in graph[node]:
-            new_node, new_cost = g[0], g[1]
+#         if fare > my_fare[node]:
+#             continue
+            
+#         for g in graph[node]:
+#             new_node, new_fare = g[0], g[1]
+#             new_fare += fare
+            
+#             if new_fare < my_fare[new_node]:
+#                 my_fare[new_node] = new_fare
+#                 heapq.heappush(hpq, [new_fare, new_node])
+    
+#     return my_fare[dst]
 
-            new_cost += cost
+# def solution(n, s, a, b, fares):
+#     answer = sys.maxsize
+    
+#     global graph, length
+#     length = n   
+#     graph = [[] for _ in range(length)]
+    
+#     for i, j, fare in fares:
+#         graph[i-1].append([j-1, fare])
+#         graph[j-1].append([i-1, fare])
         
-            if new_cost < visit[new_node]:
-                visit[new_node] = new_cost        
-                heapq.heappush(hpq, [new_cost, new_node])
-        
-    return visit[dst]
-        
+#     for i in range(length) :
+#         answer = min(answer, dij(s-1, i) + dij(i, a-1) + dij(i, b-1))
+    
+#     return answer
+
+
 def solution(n, s, a, b, fares):
-    global graph, length
-    
     answer = sys.maxsize
-    graph = [[] for _ in range(n+1)]
-    length = n
     
-    for i, j, cost in fares:
-        graph[i].append([j, cost])
-        graph[j].append([i, cost])
+    graph = [[] for _ in range(n)]
+    
+    for i, j, fare in fares:
+        graph[i-1].append([j-1, fare])
+        graph[j-1].append([i-1, fare])
+    
+    def D(src, dst):
         
-    for i in range(1, n+1):
-        answer = min(answer, dijkstra(s, i) + dijkstra(i, a) + dijkstra(i, b))
+        my_fare = [sys.maxsize for _ in range(n)]
+        my_fare[src] = 0
+        
+        hpq = [[0,src]]
+        heapq.heapify(hpq)
+        
+        while hpq:
+            fare, node = heapq.heappop(hpq)
+            
+            if fare > my_fare[node]:
+                continue
+            
+            for g in graph[node]:
+                new_node, new_fare = g[0], g[1]
+                
+                new_fare += fare
+                
+                if new_fare < my_fare[new_node]:
+                    my_fare[new_node] = new_fare
+                    heapq.heappush(hpq, [new_fare, new_node])
+        
+        
+        return my_fare[dst]
+    
+    for i in range(n) :
+        answer = min(answer , D(s-1, i) + D(i, a-1) + D(i, b-1))
+    
     
     return answer
