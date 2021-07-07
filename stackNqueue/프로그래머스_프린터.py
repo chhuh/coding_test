aeda619a90d6670a20eb274c3166bfdf212e575c
chhuh <huh1125@ajou.ac.kr>
2021-07-07 12:42:03 +0900

    new

--- stackNqueue/프로그래머스_프린터.py
+++ stackNqueue/프로그래머스_프린터.py
@@ -1,16 +1,43 @@
+# def solution(priorities, location):
+#     answer = 0
+#     loc = [i for i in range(len(priorities))]
+#     final_loc = []
+    
+#     while priorities :
+#         if priorities[0] == max(priorities):
+#             final_loc.append(loc.pop(0))
+#             priorities.pop(0)
+#         else:
+#             priorities.append(priorities.pop(0))
+#             loc.append(loc.pop(0))
+            
+#     answer = final_loc.index(location) + 1
+    
+#     return answer
+
+from collections import deque
+
 def solution(priorities, location):
     answer = 0
-    loc = [i for i in range(len(priorities))]
-    final_loc = []
     
-    while priorities :
+    mark = []
+    mine = priorities[location]
+    
+    for i, priority in enumerate(priorities):
+        mark.append((priority, i))
+    
+    p = 0
+    
+    while priorities:
         if priorities[0] == max(priorities):
-            final_loc.append(loc.pop(0))
+            p += 1
             priorities.pop(0)
+        
         else:
+            mark.append(mark.pop(p))
             priorities.append(priorities.pop(0))
-            loc.append(loc.pop(0))
-            
-    answer = final_loc.index(location) + 1
+        
+    answer = mark.index((mine,location)) + 1
     
+    print(mark)
     return answer
