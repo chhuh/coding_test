0d31df9f371df6aef3e957d0c99100ca2bad021c
chhuh <huh1125@ajou.ac.kr>
2021-09-07 13:59:49 +0900

    new

--- backtracking/백준_스타트와 링크.py
+++ backtracking/백준_스타트와 링크.py
@@ -11,8 +11,11 @@ for _ in range(n):
 visited = [0 for _ in range(n)]
 ans = sys.maxsize
 
+#재귀 함수
 def dfs(idx, cnt):
     global ans
+
+    # 정답이 될 수 있다면
     if cnt == n//2:
         team1, team2 = 0,0
         
@@ -26,12 +29,18 @@ def dfs(idx, cnt):
         
         ans = min(ans, abs(team1-team2))
         
+    # 정답이 아니라면
+    # 뻗을 수 있는 모든 자식 노드에 대하여    
     for i in range(idx, n) :
         if visited[i]:
             continue
+        # 정답후보가 될 수 있는 경우
         else:
+            # 자식 노드로 이동
             visited[i] = 1
+            # 재귀함수 + 1
             dfs(i+1, cnt+1)
+            # 부모노드로 이동
             visited[i] = 0
             
 dfs(0,0)
