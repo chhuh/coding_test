d7e5b4b0878ee9a8edbde158599f883f7ca84688
chhuh <huh1125@ajou.ac.kr>
2021-09-12 22:34:35 +0900

    new code

---
+++ DP/백준_파도반 수열 - 복사본.py
@@ -0,0 +1,14 @@
+import sys
+
+wh = [0 for i in range(101)]
+wh[1] = 1
+wh[2] = 1
+wh[3] = 1
+for i in range(0, 98):
+    wh[i + 3] = wh[i] + wh[i + 1]
+    
+tc = int(sys.stdin.readline())
+
+for i in range(tc):
+    n = int(sys.stdin.readline())
+    print(wh[n])
