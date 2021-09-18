1c172046e679fe34e693d5a35465437e6b84fc72
chhuh <huh1125@ajou.ac.kr>
2021-09-18 16:13:19 +0900

    new code

--- dividNconquer/백준_쿼드트리.py
+++ dividNconquer/백준_쿼드트리.py
@@ -2,30 +2,25 @@ import sys
 
 N = int(sys.stdin.readline())
 
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+board = [list(map(int, input())) for _ in range(N)]
 
-white = 0
-blue = 0
+def quad_tree(row, col, n):
+    num = board[row][col]
+    
+    for r in range(row, row+n):
+        for c in range(col, col+n):
+            if num != board[r][c]:
+                print("(", end ="")
+                quad_tree(row, col, n//2)
+                quad_tree(row, col+n//2, n//2)
+                quad_tree(row+n//2, col, n//2)
+                quad_tree(row+n//2, col+n//2, n//2)
+                print(")", end ="")
+                return 
+    
+    if num == 1:
+        print(1, end ="")
+    else:
+        print(0, end ="")        
 
-def divide(r, c, n):
-    global white, blue
-    color = paper[r][c]
-    for i in range(r, r+n):
-        for j in range(c, c+n):
-            if paper[i][j] != color:
-                divide(r,c,n//2)
-                divide(r,c+n//2,n//2)
-                divide(r+n//2,c,n//2)
-                divide(r+n//2,c+n//2,n//2)
-                return
-
-    if color == 0:
-        white += 1
-        return
-
-    if color == 1:
-        blue += 1
-        return
-
-divide(0,0,N)
-print("{}\n{}".format(white, blue))
+quad_tree(0,0,N)
