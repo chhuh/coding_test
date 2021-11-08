daee1eb4f6258478fa8ec46c37845f67ac1dee3d
허창현 <huh1125@ajou.ac.kr>
2021-11-08 21:34:48 +0900

    new code

--- 삼성기출/백준_시험감독.py
+++ 삼성기출/백준_시험감독.py
@@ -1,64 +1,22 @@
 import sys
-import copy
 
 N = int(sys.stdin.readline())
-K = int(sys.stdin.readline())
 
-apple = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(K)]
+room = list(map(int, sys.stdin.readline().split()))
 
-L = int(sys.stdin.readline())
+B, C = map(int, sys.stdin.readline().split())
 
-temp_arrows = [list(sys.stdin.readline().split()) for _ in range(L)]
+need = 0
 
-arrows = dict()
-
-for arrow in temp_arrows:
-    arrows[int(arrow[0])] = arrow[1]
-
-time = 0
-
-my_arrow = 0
-
-snake = [[1,1]]
-
-dr = [0,1,0,-1]
-dc = [1,0,-1,0]
-
-
-
-while True:
-    temp = copy.deepcopy(snake[-1])
-    temp = [temp[0]+dr[my_arrow], temp[1] + dc[my_arrow]]
-
-    time += 1
-
-    # 종료조건
-    if temp in snake:
-        print(time)
-        break
-
-    elif temp[0] > N or temp[1] > N or temp[0] < 1 or temp[1] < 1:
-        print(time)
-        break
-
-    # 종료가 아닌경우
-    snake.append(temp)
+for man in room:
+    need += 1
     
-    if snake[-1] in apple:
-        apple.remove(snake[-1])
+    if man <= B:
+        continue
     else:
-        snake.pop(0)
-
-    # 방향전환
-    if time in arrows.keys():
-        if arrows[time] == "D":
-            if my_arrow == 3:
-                my_arrow = 0
-            else:
-                my_arrow += 1
+        if (man-B)%C == 0:
+            need += (man-B)//C
         else:
-            if my_arrow == 0:
-                my_arrow = 3
-            else:
-                my_arrow -= 1
+            need += ((man-B)//C)+1
 
+print(need)
